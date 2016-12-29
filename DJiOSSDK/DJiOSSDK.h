66cf5c0386184dbdfd0e03d8d8d85241a8c28467
Jon Vogel <joninsky@gmail.com>
2016-12-28 16:00:56 -0800

    First Commit

---
+++ DJiOSSDK/DJiOSSDK.h
@@ -0,0 +1,19 @@
+//
+//  DJiOSSDK.h
+//  DJiOSSDK
+//
+//  Created by Jon Vogel on 12/28/16.
+//  Copyright © 2016 Jon Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DJiOSSDK.
+FOUNDATION_EXPORT double DJiOSSDKVersionNumber;
+
+//! Project version string for DJiOSSDK.
+FOUNDATION_EXPORT const unsigned char DJiOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DJiOSSDK/PublicHeader.h>
+
+
